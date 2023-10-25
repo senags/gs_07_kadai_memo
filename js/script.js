31ecc63085e5de29eab983469ec64dd457499c80
senags <yoube.23niew@gmail.com>
2023-10-25 18:49:15 +0900

    finish frames

--- js/script.js
+++ js/script.js
@@ -2,4 +2,12 @@ $('#save').on('click',function(){
   const key = $('#key').val();
   const value =$('#value').val();
   localStorage.setItem(key,value);
+
+  const html= '<tr><th>'+key+'</th><th>'+value+'</th></tr>';
+  $('#list').append(html);
+});
+
+$('#clear').on('click',function(){
+  localStorage.clear();
+  $('#list').empty();
 });
